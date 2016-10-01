4d16403ce930fcf6ed2bf9022a1675d9d4db7283
Alexandre Gomes <alegomes@gmail.com>
2016-10-01 16:05:17 -0300

    first commit

--- coletatodostweetsdeumaconta.py
+++ coletatodostweetsdeumaconta.py
@@ -1,10 +1,15 @@
 import tweepy
 
+consumer_key="VL1j9NRdaAOjtVYRtWQGfPBcJ"
+consumer_secret="6k9W95UAIhmsHyNFps7Gcu1yCeWI1iFZNdvLZOvq1vUK2KlUI7"
+access_token="14147108-KM4oIdiORvMkolmhLk2f5ym8txnwkcnbVqAWpS3Ki"
+access_token_secret="ezBfBHrmePXWcEUQEjTHXXfIJ0AtqkrdSVKmNY9DNb0Ht"
+
 # Registre sua aplicacao em https://apps.twitter.com
-consumer_key="PONHA-SUA-CONSUMER-KEY-AQUI"
-consumer_secret="PONHA-SUA-CONSUMER-SECRET-AQUI"
-access_token="PONHA-SEU-ACCESS-TOKEN-AQUI"
-access_token_secret="PONHA-SEU-ACCESS-TOKEN-SECRET-AQUI"
+# consumer_key="PONHA-SUA-CONSUMER-KEY-AQUI"
+# consumer_secret="PONHA-SUA-CONSUMER-SECRET-AQUI"
+# access_token="PONHA-SEU-ACCESS-TOKEN-AQUI"
+# access_token_secret="PONHA-SEU-ACCESS-TOKEN-SECRET-AQUI"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
