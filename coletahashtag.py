66ab14af266f326d1090be53eefa9b472ec4f65d
Alexandre Gomes <alegomes@gmail.com>
2016-10-01 15:56:23 -0300

    first tweet

---
+++ coletahashtag.py
@@ -0,0 +1,29 @@
+# Instale o tweepy, caso ainda nao o tenha.
+#   pip install tweepy
+import tweepy
+
+# Registre sua aplicacao em https://apps.twitter.com
+consumer_key="PONHA-SUA-CONSUMER-KEY-AQUI"
+consumer_secret="PONHA-SUA-CONSUMER-SECRET-AQUI"
+access_token="PONHA-SEU-ACCESS-TOKEN-AQUI"
+access_token_secret="PONHA-SEU-ACCESS-TOKEN-SECRET-AQUI"
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# http://tweepy.readthedocs.io/en/v3.5.0/api.html#API.search
+# https://dev.twitter.com/rest/reference/get/search/tweets
+# https://github.com/tweepy/tweepy/blob/master/tweepy/api.py#L1207
+tweets = api.search(q='#vemprarua')
+
+i = 1
+while tweets:
+	for tweet in tweets:
+		print('%d, %d, %s, %-20s, %s' % (i, tweet.id, tweet.created_at, tweet.author.screen_name, tweet.text))
+		i += 1
+
+
+	last_id = tweet.id
+	tweets = api.search('#vemprarua',max_id=last_id-1)
